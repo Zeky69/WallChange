a0ab09a14c1cf007b441ae8a817d1e6d6bc0610f
zakburak <zakburak@student.42.fr>
2025-12-02 12:39:06 +0100

    chore: bump version to 0.0.9

--- src/client/main.c
+++ src/client/main.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 
 #ifndef VERSION
-#define VERSION "0.0.8"
+#define VERSION "0.0.9"
 #endif
 
 static int interrupted = 0;
--- src/server/main.c
+++ src/server/main.c
@@ -7,7 +7,7 @@
 #include <openssl/sha.h>
 
 #ifndef VERSION
-#define VERSION "0.0.8"
+#define VERSION "0.0.9"
 #endif
 
 static const char *s_listen_on = "ws://0.0.0.0:8000";
